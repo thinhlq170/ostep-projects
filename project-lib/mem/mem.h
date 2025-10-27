a3c9cf0063dd37774b2585b6ee123e6cb6c176e1
Le Quang Thinh (MS/ETA-PMT) <Thinh.LeQuang2@vn.bosch.com>
2025-10-27 18:22:49 +0700

    [feature/wzipping] Adding mem.h

--- project-lib/mem/mem.h
+++ project-lib/mem/mem.h
@@ -1,1 +1,27 @@
-// TODO: implement David R Hanson's chapter 5
+#ifndef MEM_INCLUDED
+#define MEM_INCLUDED
+#include "../../project-lib/exception/except.h"
+
+// 〈exported exceptions 70〉
+// 〈exported functions 70〉
+// 〈exported macros 70〉
+
+extern const Except_T Mem_Failed;
+
+extern void *Mem_alloc(long nbytes, const char *file, int line);
+extern void *Mem_calloc(long count, long nbytes, const char *file, int line);
+extern void Mem_free(void *ptr, const char *file, int line);
+extern void *Mem_resize(void *ptr, long nbytes, const char *file, int line);
+
+#define ALLOC(nbytes) \
+    Mem_alloc((nbytes), __FILE__, __LINE__)
+#define CALLOC(count, nbytes) \
+    Mem_calloc((count), (nbytes), __FILE__, __LINE__)
+
+#define NEW(p) ((p) = ALLOC((long)sizeof *(p)))
+#define NEWO(p) ((p) = CALLOC(1, (long)sizeof *(p)))
+#define FREE(ptr) ((void)(Mem_free((ptr), __FILE__, __LINE__), (ptr) = 0))
+#define RESIZE(ptr, nbytes) ((ptr) = Mem_resize((ptr), \
+    (nbytes), __FILE__, __LINE__)) 
+
+#endif
