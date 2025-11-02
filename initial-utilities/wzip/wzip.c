3fa68a84d101540fd0c55a3c5ce936530cc15eae
thinhlq170 <thinh.lq170@gmail.com>
2025-11-02 18:26:38 +0700

    [feature/wzipping] Update wzip and pass up to test 5

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -2,15 +2,45 @@
 #include "../../common.h"
 #include "zipentry.h"
 #include "../../project-lib/mem/mem.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_ENTRY 256 // 256 kinds of characters such as alphabet and special chracters
+#define MAX_LINE 256
+#define MAX_BATCH 2048
+
+Zip_T entries[MAX_ENTRY] = {NULL}; // Initialize all entries to NULL
+size_t entryIndex = 0;
 
 
+int zipping(char *batch, const char *filename) {
+    FILE *fp;
 
-Zip_T *entries[MAX_ENTRY];
+    char *buffer = NULL;
+    fp = fopen(filename, "r");
 
-void zipping(const char *filename) {
-    
+    if (fp == NULL) {
+        fprintf(stderr, "Failed to open file\n");
+        return 1;
+    }
+    ssize_t nread;
+    size_t len;
+    while ((nread = getline(&buffer, &len, fp)) != -1) {
+        strncat(batch, buffer, nread);
+    }
+
+    FREE(buffer); // must free buffer even if the getline() failed
+    fclose(fp);
+    return 0;
+}
+
+void freeArr(Zip_T arr[]) {
+    size_t index = 0;
+
+    while (arr[index] != NULL) {
+        FREE(arr[index]);
+        index++;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -26,14 +56,55 @@ int main(int argc, char *argv[]) {
         // the shell already handled redirecting output stream by using ">"
         // We don't need to handle all tokens from user's command line
         // We only need to handle all arguments which precede the ">"
-        printf("argc: %d\n", argc);
+        char *batch = ALLOC(MAX_BATCH * sizeof(char));
+        for (size_t i = 1; i < argc; i++) {
+            zipping(batch, argv[i]);
+        }
 
-        Zip_T zip = Zip_new('a');
-        printf("character %c exists %d times\n", zip->character, zip->count);
-        FREE(zip);
+        size_t batchIdx = 0;
+
+        if (batch != NULL) {
+            char curChar = *(batch + batchIdx);
+            Zip_T entry = Zip_new(curChar);
+            batchIdx++;
+            char nextChar = *(batch + batchIdx);
+
+            if (nextChar == '\0'){
+                entries[entryIndex] = entry;
+            } else {
+                while (nextChar != '\0') {
+                    if (curChar == nextChar) {
+                        entry->count += 1;
+                        curChar = nextChar;
+                        batchIdx++;
+                        nextChar = *(batch + batchIdx);
+                    } else {
+                        entries[entryIndex++] = entry;
+                        if (nextChar != '\0') {
+                            entry = Zip_new(nextChar);
+                            curChar = nextChar;
+                            batchIdx++;
+                            nextChar = *(batch + batchIdx);
+                        }
+                        
+                    }
+                    if (nextChar == '\0')
+                        entries[entryIndex] = entry;
+                }
+            }
+        }
+        FREE(batch);
     }
 
-    free(programName);
+    size_t index = 0;
+    while (entries[index] != NULL) {
+        fwrite(entries[index], 5, 1, stdout);
+        // printf("%d%c", entries[index]->count, entries[index]->character);
+        index++;
+    }
+
+    FREE(programName);
+    freeArr(entries);
 
     return 0;
 }
